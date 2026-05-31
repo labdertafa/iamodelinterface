becc91efed29fb09264b130a26856a5c82b287c7
labrafa <ellaboratorioderafa@gmail.com>
2026-05-31 11:40:18 +0200

    Se agrega ollama para el embedding

--- src/main/java/com/laboratorio/iamodelinterface/config/ChutesEmbeddingConfiguration.java
+++ src/main/java/com/laboratorio/iamodelinterface/config/ChutesEmbeddingConfiguration.java
@@ -5,6 +5,9 @@ import com.laboratorio.chutes.embedding.config.ChutesEmbeddingApi;
 import com.laboratorio.clientapilibrary.utils.ReaderConfig;
 import com.laboratorio.iamodelinterface.util.ConnectionDataUtils;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaEmbeddingOptions;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +19,28 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class ChutesEmbeddingConfiguration {
     private final ReaderConfig config = new ReaderConfig("config//ia_models_config.properties");
 
+    @Primary
+    @Bean("ollamaEmbeddingModel")
+    public EmbeddingModel ollamaEmbeddingModel() {
+        String baseUrl = this.config.getProperty("ollama.embedding.base.url");
+        String model = this.config.getProperty("ollama.embedding.model");
+        int nDimensions = Integer.parseInt(this.config.getProperty("ollama.embedding.dimensions"));
+
+        OllamaApi api = OllamaApi.builder()
+                .baseUrl(baseUrl)
+                .build();
+
+        OllamaEmbeddingOptions options = OllamaEmbeddingOptions.builder()
+                .model(model)
+                .dimensions(nDimensions)
+                .build();
+
+        return OllamaEmbeddingModel.builder()
+                .ollamaApi(api)
+                .defaultOptions(options)
+                .build();
+    }
+
     @Bean
     public ChutesEmbeddingApi chutesEmbeddingApi() {
         String apiKey = this.config.getProperty("chutes_bearer_token");
@@ -24,7 +49,6 @@ public class ChutesEmbeddingConfiguration {
         return new ChutesEmbeddingApi(apiKey, baseUrl, uri);
     }
 
-    @Primary
     @Bean(name = "chutesEmbeddingModel")
     public EmbeddingModel embeddingModel(ChutesEmbeddingApi embeddingApi) {
         return new ChutesEmbeddingModel(embeddingApi);
@@ -33,7 +57,7 @@ public class ChutesEmbeddingConfiguration {
     @Primary
     @Bean(name = "pgVectorStore")
     public VectorStore pgVectorStore(@Qualifier("pgVectorJdbcTemplate") JdbcTemplate jdbcTemplate,
-                                    @Qualifier("chutesEmbeddingModel") EmbeddingModel embeddingModel) {
+                                    @Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel) {
         String tableName = this.config.getProperty("f1_daily_event.table.name");
         int nDimensions = Integer.parseInt(this.config.getProperty("embedding.dimensions"));
 
@@ -42,7 +66,7 @@ public class ChutesEmbeddingConfiguration {
 
     @Bean(name = "f1PgVectorStore")
     public VectorStore f1PgVectorStore(@Qualifier("supabaseF1TechJdbcTemplate") JdbcTemplate jdbcTemplate,
-                                     @Qualifier("chutesEmbeddingModel") EmbeddingModel embeddingModel) {
+                                     @Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel) {
         String tableName = this.config.getProperty("f1_daily_event.table.name");
         int nDimensions = Integer.parseInt(this.config.getProperty("embedding.dimensions"));
 
@@ -51,7 +75,7 @@ public class ChutesEmbeddingConfiguration {
 
     @Bean(name = "techPgVectorStore")
     public VectorStore techPgVectorStore(@Qualifier("supabaseTechEventJdbcTemplate") JdbcTemplate jdbcTemplate,
-                                       @Qualifier("chutesEmbeddingModel") EmbeddingModel embeddingModel) {
+                                       @Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel) {
         String tableName = this.config.getProperty("tech_daily_event.table.name");
         int nDimensions = Integer.parseInt(this.config.getProperty("embedding.dimensions"));
 
